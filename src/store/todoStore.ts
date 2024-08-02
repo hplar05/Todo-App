dc387ee4e0236d448c8bfe7a8529ebf25ad80957
Ralph Saladino <130468937+hplar05@users.noreply.github.com>
2024-08-03 02:06:24 +0800

    fix: folder structure added src

---
+++ src/store/todoStore.ts
@@ -0,0 +1,12 @@
+import { create } from "zustand";
+import { TodoStore } from "./types";
+
+
+const useTodoStore = create<TodoStore>(set => ({
+    todos: [],
+    addTodo: (text) => set((state) => ({todos: [...state.todos, {text, completed:false, id: Date.now()}]})),
+    removeTodo: (id) => set((state) => ({todos: state.todos.filter(t => t.id !== id)})),
+    toggleTodo: (id) => set((state) => ({todos: state.todos.map(t => t.id === id ? {...t, completed: !t.completed}: t)}))
+}))
+
+export default useTodoStore
---
+++ src/store/types.ts
@@ -0,0 +1,12 @@
+export interface Todo {
+    id: number;
+    text: string;
+    completed: boolean;
+  }
+  
+  export interface TodoStore {
+    todos: Todo[];
+    addTodo: (text: string) => void;
+    removeTodo: (id: number) => void;
+    toggleTodo: (id: number) => void,
+  }
