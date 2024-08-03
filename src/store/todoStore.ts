5c2a10428e314c924472a85451c93bc5da103b50
Ralph Saladino <130468937+hplar05@users.noreply.github.com>
2024-08-04 01:05:10 +0800

    feat: async storage and state storage of zustand

--- src/store/todoStore.ts
+++ src/store/todoStore.ts
@@ -1,15 +1,24 @@
 import { create } from "zustand";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { persist, StateStorage } from 'zustand/middleware';
 import { TodoStore } from "../app/constants/types";
 
-
-const useTodoStore = create<TodoStore>(set => ({
-    todos: [],
-    addTodo: (text) => set((state) => ({todos: [...state.todos, {text, completed:false, id: Date.now()}]})),
-    removeTodo: (id) => set((state) => ({todos: state.todos.filter(t => t.id !== id)})),
-    editTodo: (id, newTodo) => set((state) => ({
+const useTodoStore = create<TodoStore>()(
+  persist(
+    (set) => ({
+      todos: [],
+      addTodo: (text) => set((state) => ({ todos: [...state.todos, { text, completed: false, id: Date.now() }] })),
+      removeTodo: (id) => set((state) => ({ todos: state.todos.filter(t => t.id !== id) })),
+      editTodo: (id, newTodo) => set((state) => ({
         todos: state.todos.map((t) => t.id === id ? { ...t, text: newTodo } : t),
-    })),
-    toggleTodo: (id) => set((state) => ({todos: state.todos.map(t => t.id === id ? {...t, completed: !t.completed}: t)}))
-}))
+      })),
+      toggleTodo: (id) => set((state) => ({ todos: state.todos.map(t => t.id === id ? { ...t, completed: !t.completed } : t) }))
+    }),
+    {
+      name: 'todo-storage',
+      getStorage: (): StateStorage => AsyncStorage,
+    }
+  )
+);
 
-export default useTodoStore
+export default useTodoStore;
